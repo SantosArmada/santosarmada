ccaf1b1ad433ef92dc80bcbe0227f143095d5046
Santos Armada <santosarmada@protonmail.com>
2026-07-01 08:37:02 -0700

    Fix typo in import statement

--- santos-armada-title.js
+++ santos-armada-title.js
@@ -1,4 +1,4 @@
-iimport * as THREE from 'three';
+import * as THREE from 'three';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
