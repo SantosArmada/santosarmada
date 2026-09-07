f306acb8f64a1946c93c50d11dca229d0c26025d
Santos Armada <santosarmada@protonmail.com>
2026-09-06 21:53:06 -0700

    Increase El rayo de luna player contrast

--- Audiolibros/titulos/el-rayo-de-luna.js
+++ Audiolibros/titulos/el-rayo-de-luna.js
@@ -43,9 +43,9 @@ var Framer = {
         var dy2 = parseInt(this.scene.cy + y2);
 
         var gradient = this.context.createLinearGradient(dx1, dy1, dx2, dy2);
-        gradient.addColorStop(0, '#1C2D4A');
-        gradient.addColorStop(0.6, '#1C2D4A');
-        gradient.addColorStop(1, '#ECDFC0');
+        gradient.addColorStop(0, '#FFFFFF');
+        gradient.addColorStop(0.6, '#FFFFFF');
+        gradient.addColorStop(1, '#FFFFFF');
         this.context.beginPath();
         this.context.strokeStyle = gradient;
         this.context.lineWidth = 2;
@@ -61,7 +61,7 @@ var Framer = {
     drawEdging: function () {
         this.context.save();
         this.context.beginPath();
-        this.context.strokeStyle = 'rgba(28, 45, 74, 0.5)';
+        this.context.strokeStyle = 'rgba(255, 255, 255, 0.5)';
         this.context.lineWidth = 1;
 
         var offset = Tracker.lineWidth / 2;
@@ -198,7 +198,7 @@ var Tracker = {
 
     drawArc: function () {
         this.context.save();
-        this.context.strokeStyle = 'rgba(28, 45, 74, 0.8)';
+        this.context.strokeStyle = 'rgba(255, 255, 255, 0.8)';
         this.context.beginPath();
         this.context.lineWidth = this.lineWidth;
 
@@ -270,7 +270,7 @@ var Scene = {
     canvasConfigure: function () {
         this.canvas = document.querySelector('canvas');
         this.context = this.canvas.getContext('2d');
-        this.context.strokeStyle = '#1C2D4A';
+        this.context.strokeStyle = '#FFFFFF';
         this.calculateSize();
     },
 
@@ -409,7 +409,7 @@ var Controls = {
     drawPic: function () {
         this.context.save();
         this.context.beginPath();
-        this.context.fillStyle = 'rgba(28, 45, 74, 0.9)';
+        this.context.fillStyle = 'rgba(255, 255, 255, 0.9)';
         this.context.lineWidth = 1;
         var x = Tracker.r / Math.sqrt(Math.pow(Math.tan(Tracker.angle), 2) + 1);
         var y = Math.sqrt(Tracker.r * Tracker.r - x * x);
