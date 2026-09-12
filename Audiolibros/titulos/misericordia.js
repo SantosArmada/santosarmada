6c029022cb3b65f36eb3cfac4fb7b79e2ec4256c
Santos Armada <santosarmada@protonmail.com>
2026-09-11 18:47:52 -0700

    Redesign Misericordia audiobook palette

--- Audiolibros/titulos/misericordia.js
+++ Audiolibros/titulos/misericordia.js
@@ -43,9 +43,9 @@ var Framer = {
         var dy2 = parseInt(this.scene.cy + y2);
 
         var gradient = this.context.createLinearGradient(dx1, dy1, dx2, dy2);
-        gradient.addColorStop(0, '#6B1616');
-        gradient.addColorStop(0.6, '#6B1616');
-        gradient.addColorStop(1, '#ECDFC0');
+        gradient.addColorStop(0, '#3F7894');
+        gradient.addColorStop(0.6, '#3F7894');
+        gradient.addColorStop(1, '#F2DFB8');
         this.context.beginPath();
         this.context.strokeStyle = gradient;
         this.context.lineWidth = 2;
@@ -61,7 +61,7 @@ var Framer = {
     drawEdging: function () {
         this.context.save();
         this.context.beginPath();
-        this.context.strokeStyle = 'rgba(107, 22, 22, 0.5)';
+        this.context.strokeStyle = 'rgba(63, 120, 148, 0.5)';
         this.context.lineWidth = 1;
 
         var offset = Tracker.lineWidth / 2;
@@ -198,7 +198,7 @@ var Tracker = {
 
     drawArc: function () {
         this.context.save();
-        this.context.strokeStyle = 'rgba(107, 22, 22, 0.8)';
+        this.context.strokeStyle = 'rgba(63, 120, 148, 0.8)';
         this.context.beginPath();
         this.context.lineWidth = this.lineWidth;
 
@@ -270,7 +270,7 @@ var Scene = {
     canvasConfigure: function () {
         this.canvas = document.querySelector('canvas');
         this.context = this.canvas.getContext('2d');
-        this.context.strokeStyle = '#6B1616';
+        this.context.strokeStyle = '#3F7894';
         this.calculateSize();
     },
 
@@ -409,7 +409,7 @@ var Controls = {
     drawPic: function () {
         this.context.save();
         this.context.beginPath();
-        this.context.fillStyle = 'rgba(107, 22, 22, 0.9)';
+        this.context.fillStyle = 'rgba(128, 96, 59, 0.95)';
         this.context.lineWidth = 1;
         var x = Tracker.r / Math.sqrt(Math.pow(Math.tan(Tracker.angle), 2) + 1);
         var y = Math.sqrt(Tracker.r * Tracker.r - x * x);
